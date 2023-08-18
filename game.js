cddb6a40a0c8d098f380efb10b381a6fae152653
RichRich087 <>
2023-08-18 19:01:57 +0000

    added background changes

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 //IMPORTANT: Make sure to use Kaboom version 0.5.0 for this game by adding the correct script tag in the HTML file.
 
 kaboom({
-  global: true,
-  fullscreen: true,
-  scale: 2,
-  debug: true,
-  clearColor: [0, 0, 0, 1],
+    global: true,
+    fullscreen: true,
+    scale: 2,
+    debug: true,
+    clearColor: [0, 0, 0, 1],
 })
 
 // Speed identifiers
@@ -20,8 +20,12 @@ const ENEMY_SPEED = 20
 
 let isJumping = true
 
-loadSprite('background', '/assets/images/background.png')
-loadSprite('turtle', 'assets/images/turtle.png') // add turtle
+loadSprite('background1', '/assets/images/background.png')
+loadSprite('background2', '/assets/images/background2.png')
+loadSprite('background3', '/assets/images/background3.png')
+
+
+
 loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
 loadSprite('evil-shroom', 'KPO3fR9.png')
@@ -42,277 +46,226 @@ loadSprite('blue-steel', 'gqVoI2b.png')
 loadSprite('blue-evil-shroom', 'SvV4ueD.png')
 loadSprite('blue-surprise', 'RMqCc1G.png')
 
-scene("game", ({
-  level,
-  score
-}) => {
-  layers(['bg', 'obj', 'ui'], 'obj')
 
-  // Add the background
-  add([
-    sprite('background'),
-    layer('bg'),
-    pos(0, 0),
-    scale(4.9, .495) // Scale the background to fit the screen
-  ])
 
-  const maps = [
+scene("game", ({ level, score }) => {
+    layers(['bg', 'obj', 'ui'], 'obj')
 
-    [
-      '                                                                                                     ',
-      '                                                                                                     ',
-      '                                                                                                     ',
-      '                                                                                                     ',
-      '                                                                                                     ',
-      '    %   =*=%=                        %=*=%=                                                        % ',
-      '                                                                                                     ',
-      '                            -+                                     -+                         -+     ',
-      '            ^        ^   ^  ()   ^                         ^    ^  ()                         ()     ',
-      '=================  ==========================  ======  ==================   =============== =========',
-    ],
-    [
-      '£                                                                                                      £',
-      '£                                                                                                      £',
-      '£                                                                                                      £',
-      '£                                                                                                      £',
-      '£                                                                                                      £',
-      '£  @@@@@@   =*=%=            x x                                     @@@=*=%=                          £',
-      '£                          x x x     =*=%=                                                             £',
-      '£                        x x x x  x                                                                  -+£',
-      '£            &     & z  x x x x x  x                      &   &            &  z       &           & ()£',
-      '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  !!!!  !!!!!!!!!!!!! !!!!!!!!!!!!!!!!!!!!    !!!!!!!!!!! ',
-    ],
-    [
-      '                                                                                 ',
-      '                                                                                 ',
-      '                                                                                 ',
-      '                                                                                 ',
-      '                                                                                 ',
-      '     %   =*=%=                                                                   ',
-      '                                                                                 ',
-      '                            -+                                                   ',
-      '           ^         ^   ^  ()   ^                         ^    ^                ',
-      '=================  ==========================  ======  ==================   =====',
-    ],
-    [
-      '£                                       £',
-      '£                                       £',
-      '£                                       £',
-      '£                                       £',
-      '£                                       £',
-      '£        @@@@@@              x x        £',
-      '£                          x x x        £',
-      '£                        x x x x  x   -+£',
-      '£               z   z  x x x x x  x   ()£',
-      '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
-    ],
-    /* level by q 
-problems: evil shrooms don't fall; evil shrooms should move back and forth
-requires: mushroom to also increase jump force
-requires: finish line to go to next lvl*/
-    [
-      '                                                                                                                    ',
-      '                                                  *                           $$$$$$                                ',
-      '                                                                            ===========                             ',
-      '                                               =======                                                              ',
-      '                 $$$                                                                                                ',
-      '               =======                                   %%%%%         ^                                            ',
-      '                                                                 =======                           ^                ',
-      '          ====          =%%%%%=                                =========        *         ==========                ',
-      '                                            ^                ===========                               $$           ',
-      '=========         ^             ^        ======            =============                             ======         ',
-      '           ========  ====================      ======  ==================   =====                            =======',
-    ],
-    // level design by james
-    [
-        '!                                                                                                                   ',
-        '!                                                                                                    $              ',
-        '!                                                                                                ==  =              ',
-        '!                                                                  $$$                       ==                     ',
-        '!                      ^                                          }}}}}     *%%                          !  !       ',
-        '!                   =*===                                                                 $              !$$!       ',
-        '!               $                                              }         =======          x              !$$!       ',
-        '!              &x                   xx                      }                            xxx             !$$!       ',
-        '!             $xxx$     ===   %%%   ()               =%%=                               xxxxx&           !  !       ',
-        '!            $xxxxx$                ()xx                              x                xxxxxxx                  -+  ',
-        '!            xxxxxxx                ()()             ^ ^             xxx        &     xxxxxxxxx                 ()  ',
-        '==========================    ==============================         ===============================================',
-      ],
-  ]
-
-  const levelCfg = {
-    width: 20,
-    height: 20,
-    '=': [sprite('block'), solid()],
-    '$': [sprite('coin'), 'coin'],
-    '%': [sprite('surprise'), solid(), 'coin-surprise'],
-    '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
-    '}': [sprite('unboxed'), solid()],
-    '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
-    ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
-    '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
-    '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
-    '^': [sprite('evil-shroom'), solid(), 'dangerous', patrol(ENEMY_SPEED)],
-    '#': [sprite('mushroom'), solid(), 'mushroom', body()],
-    '!': [sprite('blue-block'), solid(), scale(0.5)],
-    '£': [sprite('blue-brick'), solid(), scale(0.5)],
-    'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous', patrol(ENEMY_SPEED)],
-    '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
-    'x': [sprite('blue-steel'), solid(), scale(0.5)],
-    '&': [sprite('turtle'), solid(), 'turtle', patrol(ENEMY_SPEED)],
-  }
-
-  const gameLevel = addLevel(maps[level], levelCfg)
-
-  const scoreLabel = add([
-    text(score),
-    pos(30, 6),
-    layer('ui'),
-    {
-      value: score,
+    let backgroundSprite;
+    switch (level) {
+        case 0:
+            backgroundSprite = 'background1';
+            break;
+        case 1:
+            backgroundSprite = 'background2';
+            break;
+        case 2:
+            backgroundSprite = 'background3';
+            break;
+        default:
+            backgroundSprite = 'background1';
     }
-  ])
+    // Add the background
+    add([
 
-  add([text('level ' + parseInt(level + 1)), pos(40, 6)])
+        sprite(backgroundSprite),
+        layer('bg'),
+        pos(0, 0),
+        scale(1.9, .495) // Scale the background to fit the screen
+    ])
 
-  function big() {
-    let timer = 0
-    let isBig = false
-    return {
-      update() {
-        if (isBig) {
-          CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
-          timer -= dt()
-          if (timer <= 0) {
-            this.smallify()
-          }
+    const maps = [
+        [
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '     %   =*=%=                        ',
+            '                                      ',
+            '                            -+        ',
+            '                    ^   ^   ()        ',
+            '==============================   =====',
+        ],
+        [
+            '                                                                                 ',
+            '                                                                                 ',
+            '                                                                                 ',
+            '                                                                                 ',
+            '                                                                                 ',
+            '     %   =*=%=                                                                   ',
+            '                                                                                 ',
+            '                            -+                                                   ',
+            '           ^         ^   ^  ()   ^                         ^    ^                ',
+            '=================  ==========================  ======  ==================   =====',
+        ],
+        [
+            '£                                       £',
+            '£                                       £',
+            '£                                       £',
+            '£                                       £',
+            '£                                       £',
+            '£        @@@@@@              x x        £',
+            '£                          x x x        £',
+            '£                        x x x x  x   -+£',
+            '£               z   z  x x x x x  x   ()£',
+            '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+        ]
+    ]
+
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('block'), solid()],
+        '$': [sprite('coin'), 'coin'],
+        '%': [sprite('surprise'), solid(), 'coin-surprise'],
+        '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+        '}': [sprite('unboxed'), solid()],
+        '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
+        ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
+        '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
+        '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
+        '^': [sprite('evil-shroom'), solid(), 'dangerous'],
+        '#': [sprite('mushroom'), solid(), 'mushroom', body()],
+        '!': [sprite('blue-block'), solid(), scale(0.5)],
+        '£': [sprite('blue-brick'), solid(), scale(0.5)],
+        'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
+        '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
+        'x': [sprite('blue-steel'), solid(), scale(0.5)],
+
+    }
+
+    const gameLevel = addLevel(maps[level], levelCfg)
+
+    const scoreLabel = add([
+        text(score),
+        pos(30, 6),
+        layer('ui'),
+        {
+            value: score,
         }
-      },
-      isBig() {
-        return isBig
-      },
-      smallify() {
-        this.scale = vec2(1)
-        CURRENT_JUMP_FORCE = JUMP_FORCE
-        timer = 0
-        isBig = false
-      },
-      biggify(time) {
-        this.scale = vec2(2)
-        timer = time
-        isBig = true
-        CURRENT_JUMP_FORCE = BIG_JUMP_FORCE;
-      }
+    ])
+
+    add([text('level ' + parseInt(level + 1)), pos(40, 6)])
+
+    function big() {
+        let timer = 0
+        let isBig = false
+        return {
+            update() {
+                if (isBig) {
+                    CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+                    timer -= dt()
+                    if (timer <= 0) {
+                        this.smallify()
+                    }
+                }
+            },
+            isBig() {
+                return isBig
+            },
+            smallify() {
+                this.scale = vec2(1)
+                CURRENT_JUMP_FORCE = JUMP_FORCE
+                timer = 0
+                isBig = false
+            },
+            biggify(time) {
+                this.scale = vec2(2)
+                timer = time
+                isBig = true
+            }
+        }
     }
-  }
 
-  const player = add([
-    sprite('mario'), solid(),
-    pos(30, 0),
-    body(),
-    big(),
-    origin('bot')
-  ])
+    const player = add([
+        sprite('mario'), solid(),
+        pos(30, 0),
+        body(),
+        big(),
+        origin('bot')
+    ])
 
-  action('mushroom', (m) => {
-    m.move(20, 0)
-  })
-
-  player.on("headbump", (obj) => {
-    if (obj.is('coin-surprise')) {
-      gameLevel.spawn('$', obj.gridPos.sub(0, 1))
-      destroy(obj)
-      gameLevel.spawn('}', obj.gridPos.sub(0, 0))
-    }
-    if (obj.is('mushroom-surprise')) {
-      gameLevel.spawn('#', obj.gridPos.sub(0, 1))
-      destroy(obj)
-      gameLevel.spawn('}', obj.gridPos.sub(0, 0))
-    }
-  })
-
-  player.collides('mushroom', (m) => {
-    destroy(m)
-    player.biggify(6)
-  })
-
-  player.collides('coin', (c) => {
-    destroy(c)
-    scoreLabel.value++
-    scoreLabel.text = scoreLabel.value
-  })
-
-  action('dangerous', (d) => {
-    d.move(-ENEMY_SPEED, 0)
-  })
-
-  player.collides('dangerous', (d) => {
-    if (isJumping) {
-      destroy(d)
-    } else {
-      go('lose', {
-        score: scoreLabel.value
-      })
-    }
-  })
-
-  player.action(() => {
-    camPos(player.pos)
-    if (player.pos.y >= FALL_DEATH) {
-      go('lose', {
-        score: scoreLabel.value
-      })
-    }
-  })
-
-  player.collides('pipe', () => {
-    keyPress('down', () => {
-      go('game', {
-        level: (level + 1) % maps.length,
-        score: scoreLabel.value
-      })
+    action('mushroom', (m) => {
+        m.move(20, 0)
     })
-  })
-  // add turtle
-  player.collides('turtle', (t) => {
-    if (player.grounded()) {
-      destroy(t);
-      go('lose', {
-        score: scoreLabel.value
-      });
-    } else {
-      destroy(t);
-    }
-  });
 
-  action('turtle', (t) => {
-    t.move(-ENEMY_SPEED, 0);
-  });
+    player.on("headbump", (obj) => {
+        if (obj.is('coin-surprise')) {
+            gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+        }
+        if (obj.is('mushroom-surprise')) {
+            gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+        }
+    })
 
-  keyDown('right', () => {
-    player.move(MOVE_SPEED, 0)
-  })
+    player.collides('mushroom', (m) => {
+        destroy(m)
+        player.biggify(6)
+    })
 
-  keyDown('left', () => {
-    player.move(-MOVE_SPEED, 0)
-  })
+    player.collides('coin', (c) => {
+        destroy(c)
+        scoreLabel.value++
+        scoreLabel.text = scoreLabel.value
+    })
 
-  keyPress('space', () => {
-    if (player.grounded()) {
-      isJumping = true
-      player.jump(CURRENT_JUMP_FORCE)
-    }
-  })
+    action('dangerous', (d) => {
+        d.move(-ENEMY_SPEED, 0)
+    })
 
+    player.collides('dangerous', (d) => {
+        if (isJumping) {
+            destroy(d)
+        } else {
+            go('lose', { score: scoreLabel.value })
+        }
+    })
+
+    player.action(() => {
+        camPos(player.pos)
+        if (player.pos.y >= FALL_DEATH) {
+            go('lose', { score: scoreLabel.value })
+        }
+    })
+
+    player.collides('pipe', () => {
+        keyPress('down', () => {
+            go('game', {
+                level: (level + 1) % maps.length,
+                score: scoreLabel.value
+            })
+        })
+    })
+
+    keyDown('left', () => {
+        player.move(-MOVE_SPEED, 0)
+    })
+
+    keyDown('right', () => {
+        player.move(MOVE_SPEED, 0)
+    })
+
+    player.action(() => {
+        if (player.grounded()) {
+            isJumping = false
+        }
+    })
+
+    keyPress('space', () => {
+        if (player.grounded()) {
+            isJumping = true
+            player.jump(CURRENT_JUMP_FORCE)
+        }
+    })
 })
 
-scene('lose', ({
-  score
-}) => {
-  add([text(score, 32), origin('center'), pos(width() / 2, height() / 2)])
+scene('lose', ({ score }) => {
+    add([text(score, 32), origin('center'), pos(width() / 2, height() / 2)])
 })
 
-start("game", {
-  level: 0,
-  score: 0
-})
+start("game", { level: 0, score: 0 })
